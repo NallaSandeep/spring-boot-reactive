1c6058865ae3674638f32de4d8a4b23599c8b801
NallaSandeep <nallasandeep056@gmail.com>
2022-07-05 01:05:38 +0530

    Added doc comments

--- src/main/java/com/ftd/springboot/webflux/reactive/FluxEx.java
+++ src/main/java/com/ftd/springboot/webflux/reactive/FluxEx.java
@@ -7,11 +7,16 @@ import java.util.stream.Stream;
 
 public class FluxEx {
 
+    /**
+     * Flux emits zero or more elements
+     */
     public static void main(String[] args) {
         Flux.fromIterable(Stream.of("Spring", "Reactive").collect(Collectors.toList()))
                 .subscribe(System.out::println);
     }
-    //Output
-    //Spring
-    //Reactive
+    /**
+    Output:
+        Spring
+        Reactive
+    */
 }
--- src/main/java/com/ftd/springboot/webflux/reactive/MonoEx.java
+++ src/main/java/com/ftd/springboot/webflux/reactive/MonoEx.java
@@ -4,11 +4,16 @@ import reactor.core.publisher.Mono;
 
 public class MonoEx {
 
+    /**
+     * Mono emits zero or one element
+     */
     public static void main(String[] args) {
         Mono.justOrEmpty("Spring")
                 .subscribe(System.out::println);
     }
 
-    // Output:
-    // Spring
+    /**
+     Output:
+        Spring
+    */
 }
